de620b6c45704aeb2d662561b2612f12355aba18
Claudia <claudiateresprieto@gmail.com>
2023-12-04 09:11:02 +0100

    [#3]: move pain levels content into its own file

--- src/PainTracker/PainTracker.tsx
+++ src/PainTracker/PainTracker.tsx
@@ -2,52 +2,7 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-paper';
 import {SurfaceButton} from './components/SurfaceButton';
-
-interface IPainLevels {
-  level: string;
-  helperText: string;
-  value: string;
-  color: string;
-}
-
-const painLevels: IPainLevels[] = [
-  {
-    level: '0',
-    helperText: 'Amazing! 😀',
-    value: '0',
-    color: 'green',
-  },
-  {
-    level: '1',
-    helperText: 'Okay 🙂',
-    value: '1',
-    color: 'olive',
-  },
-  {
-    level: '2',
-    helperText: 'I feel some pain 😕',
-    value: '2',
-    color: 'salmon',
-  },
-  {
-    level: '3',
-    helperText: "I'm in pain most of the time 😵‍💫",
-    value: '3',
-    color: 'blue',
-  },
-  {
-    level: '4',
-    helperText: 'It can get instense 🥴',
-    value: '4',
-    color: 'purple',
-  },
-  {
-    level: '5',
-    helperText: 'This is unbearable 😫',
-    value: '5',
-    color: 'red',
-  },
-];
+import {painLevels} from './content';
 
 export const PainTracker = () => {
   return (
---
+++ src/PainTracker/content.ts
@@ -0,0 +1,45 @@
+interface IPainLevels {
+  level: string;
+  helperText: string;
+  value: string;
+  color: string;
+}
+
+export const painLevels: IPainLevels[] = [
+  {
+    level: '0',
+    helperText: 'Amazing! 😀',
+    value: '0',
+    color: 'green',
+  },
+  {
+    level: '1',
+    helperText: 'Okay 🙂',
+    value: '1',
+    color: 'olive',
+  },
+  {
+    level: '2',
+    helperText: 'I feel some pain 😕',
+    value: '2',
+    color: 'salmon',
+  },
+  {
+    level: '3',
+    helperText: "I'm in pain most of the time 😵‍💫",
+    value: '3',
+    color: 'blue',
+  },
+  {
+    level: '4',
+    helperText: 'It can get instense 🥴',
+    value: '4',
+    color: 'purple',
+  },
+  {
+    level: '5',
+    helperText: 'This is unbearable 😫',
+    value: '5',
+    color: 'red',
+  },
+];
