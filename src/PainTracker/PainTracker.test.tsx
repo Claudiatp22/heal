1d5c5e7bef384dca8e62f7eaaeee917b7ee6dcfe
Claudia <claudiateresprieto@gmail.com>
2023-12-07 13:34:31 +0100

    [#3]: display registered pain level if there is one

--- src/PainTracker/PainTracker.test.tsx
+++ src/PainTracker/PainTracker.test.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import {render, screen} from '@testing-library/react-native';
 import {PainTracker} from './PainTracker';
+import {fireEvent, render, screen} from '@testing-library/react-native';
 
 describe('PainTracker component', () => {
   it('should ask the user how is her pain feeling today', () => {
     render(<PainTracker />);
 
-    expect(screen.getByText('How is your pain level today?')).toBeVisible();
+    expect(screen.getByText(/how is your pain level today?/i)).toBeVisible();
   });
 
   it('should display several pain levels to choose from', () => {
@@ -19,5 +19,26 @@ describe('PainTracker component', () => {
     expect(screen.getByText(/I'm in pain most of the time 😵‍💫/i)).toBeVisible();
     expect(screen.getByText(/It can get instense 🥴/i)).toBeVisible();
     expect(screen.getByText(/This is unbearable 😫/i)).toBeVisible();
+  });
+
+  it('should display user`s registered pain level', () => {
+    render(<PainTracker />);
+
+    fireEvent.press(screen.getByText(/It can get instense 🥴/i));
+
+    expect(
+      screen.getByText(/Today you're feeling: It can get instense 🥴/i),
+    ).toBeVisible();
+  });
+
+  it('should not ask the user to register pain level if there is one', () => {
+    render(<PainTracker />);
+
+    fireEvent.press(screen.getByText(/Amazing! 😀/i));
+
+    expect(
+      screen.queryByText(/how is your pain level today?/i),
+    ).not.toBeOnTheScreen();
+    expect(screen.queryByText(/Okay 🙂/i)).not.toBeOnTheScreen();
   });
 });
--- src/PainTracker/PainTracker.tsx
+++ src/PainTracker/PainTracker.tsx
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-paper';
 import {SurfaceButton} from './components/SurfaceButton';
 import {painLevels} from './content';
 
+interface IRegisteredPainLevel {
+  level: string;
+  helperText: string;
+  value: string;
+  color: string;
+}
+
 export const PainTracker = () => {
+  const [registeredPainLevel, setRegisteredPainLevel] =
+    useState<IRegisteredPainLevel>();
+
+  const registerPainLevel = useCallback((level: string) => {
+    const painLevel = painLevels.find(l => l.level === level);
+    painLevel && setRegisteredPainLevel(painLevel);
+  }, []);
+
   return (
     <View style={styles.mainContainer}>
-      <Text style={styles.questionText}>How is your pain level today?</Text>
-      <View>
-        {painLevels.map((l, index) => (
-          <SurfaceButton
-            key={index}
-            text={l.level}
-            helperText={l.helperText}
-            value={l.value}
-            color={l.color}
-            onSubmit={(value: string) =>
-              console.log(`Registered pain level ${value}`)
-            }
-          />
-        ))}
-      </View>
+      {registeredPainLevel ? (
+        <Text>Today you're feeling: {registeredPainLevel.helperText}</Text>
+      ) : (
+        <View>
+          <Text style={styles.questionText}>How is your pain level today?</Text>
+          <View>
+            {painLevels.map((l, index) => (
+              <SurfaceButton
+                key={index}
+                text={l.level}
+                helperText={l.helperText}
+                value={l.value}
+                color={l.color}
+                onSubmit={registerPainLevel}
+              />
+            ))}
+          </View>
+        </View>
+      )}
     </View>
   );
 };
